c11025012cba4519fc72d42ce19ff283dcb059e5
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-30 20:59:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include "lib.h"
+using namespace std;
 
+bool controllo (char c){
+    if (c >='A' and c <='Z'){
+        return true;
+    } else if(c >='a' and c<='z'){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+char convertitore(char c) {
+    char risultato;
+    if (c >= 'A' and c <= 'Z') {
+        risultato = c + 32;
+    } else if (c >= 'a' and c <= 'z') {
+        risultato = c - 32;
+        return risultato;
+    }
+}
