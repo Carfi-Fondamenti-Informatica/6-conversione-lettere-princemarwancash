2746934963548769ce0b57ae1919154afe999d45
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-30 19:15:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,127 @@
 #include <iostream>
-#include "lib.h"
 using namespace std;
+bool controllo (char a){
+    if (a=='a' or a=='b' or a=='c' or a=='d' or a=='e' or a=='f' or a=='g' or a=='h' or a=='i'){
+        return true;
+    } else if(a=='j' or a=='k' or a=='w' or a=='x' or a=='y'){
+        return true;
+    } else if(a=='l' or a=='m' or a=='n' or a=='o' or a=='p' or a=='q' or a=='r' or a=='s' or a=='t' or a=='v' or a=='z'){
+        return true;
+    } else if(a=='A' or a=='B' or a=='C' or a=='D' or a=='E' or a=='F' or a=='G' or a=='H' or a=='I'){
+        return true;
+    } else if(a=='J' or a=='K' or a=='W' or a=='X' or a=='Y'){
+        return true;
+    } else if(a=='L' or a=='M' or a=='N' or a=='O' or a=='P' or a=='Q' or a=='R' or a=='S' or a=='T' or a=='V' or a=='Z'){
+        return true;
+    } else {
+        return false;
+    }
+}
 
+void convertitore (char a){
+    if (a=='a'){
+        cout << 'A' << endl;
+    } else if (a=='b'){
+        cout << 'B' << endl;
+    } else if (a=='c'){
+        cout << 'C' << endl;
+    } else if (a=='d'){
+        cout << 'D' << endl;
+    } else if (a=='e'){
+        cout << 'E' << endl;
+    } else if (a=='f'){
+        cout << 'F' << endl;
+    } else if (a=='g'){
+        cout << 'G' << endl;
+    } else if (a=='h'){
+        cout << 'H' << endl;
+    } else if (a=='i'){
+        cout << 'I' << endl;
+    } else if (a=='j'){
+        cout << 'J' << endl;
+    } else if (a=='k'){
+        cout << 'K' << endl;
+    } else if (a=='l'){
+        cout << 'L' << endl;
+    } else if (a=='m'){
+        cout << 'M' << endl;
+    } else if (a=='n'){
+        cout << 'N' << endl;
+    } else if (a=='o'){
+        cout << 'O' << endl;
+    } else if (a=='p'){
+        cout << 'P' << endl;
+    } else if (a=='q'){
+        cout << 'Q' << endl;
+    } else if (a=='r'){
+        cout << 'R' << endl;
+    } else if (a=='s'){
+        cout << 'S' << endl;
+    } else if (a=='t'){
+        cout << 'T' << endl;
+    } else if (a=='v'){
+        cout << 'V' << endl;
+    } else if (a=='w'){
+        cout << 'W' << endl;
+    } else if (a=='x'){
+        cout << 'X' << endl;
+    } else if (a=='y'){
+        cout << 'Y' << endl;
+    } else if (a=='z'){
+        cout << 'Z' << endl;
+    } else if (a=='A'){
+        cout << 'a' << endl;
+    } else if (a=='B'){
+        cout << 'b' << endl;
+    } else if (a=='C'){
+        cout << 'c' << endl;
+    } else if (a=='D'){
+        cout << 'd' << endl;
+    } else if (a=='E'){
+        cout << 'e' << endl;
+    } else if (a=='F'){
+        cout << 'f' << endl;
+    } else if (a=='G'){
+        cout << 'g' << endl;
+    } else if (a=='H'){
+        cout << 'h' << endl;
+    } else if (a=='I'){
+        cout << 'i' << endl;
+    } else if (a=='J'){
+        cout << 'j' << endl;
+    } else if (a=='K'){
+        cout << 'k' << endl;
+    } else if (a=='L'){
+        cout << 'l' << endl;
+    } else if (a=='M'){
+        cout << 'm' << endl;
+    } else if (a=='N'){
+        cout << 'n' << endl;
+    } else if (a=='O'){
+        cout << 'o' << endl;
+    } else if (a=='P'){
+        cout << 'p' << endl;
+    } else if (a=='Q'){
+        cout << 'q' << endl;
+    } else if (a=='R'){
+        cout << 'r' << endl;
+    } else if (a=='S'){
+        cout << 's' << endl;
+    } else if (a=='T'){
+        cout << 't' << endl;
+    } else if (a=='V'){
+        cout << 'v' << endl;
+    } else if (a=='W'){
+        cout << 'w' << endl;
+    } else if (a=='X'){
+        cout << 'x' << endl;
+    } else if (a=='Y'){
+        cout << 'y' << endl;
+    } else if (a=='Z'){
+        cout << 'z' << endl;
+    }
+
+}
 int main (){
     char a;
     cin >> a;
