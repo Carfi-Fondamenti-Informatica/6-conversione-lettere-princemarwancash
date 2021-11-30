850df40d9411dea32c81d1d965011f82cfb5d005
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-30 19:11:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main (){
+    char a;
+    cin >> a;
+    cout << controllo(a) << endl;
+    if (controllo(a)){
+        convertitore(a);
+    } else {
+        cout << "Errore" << endl;
+    }
+    return 0;
 }
