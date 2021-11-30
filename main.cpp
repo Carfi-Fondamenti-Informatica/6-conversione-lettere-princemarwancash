c6745f506dc8f3e0fa322e6713d8070ca6b01f77
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-30 22:49:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,14 @@
 #include <iostream>
+#include "lib.h"
 using namespace std;
-
-bool controllo (char c){
-    if (c >='A' and c <='Z'){
-        return true;
-    } else if(c >='a' and c<='z'){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-char convertitore(char c) {
-    char risultato;
-    if (c >= 'A' and c <= 'Z') {
-        risultato = c + 32;
-    } else if (c >= 'a' and c <= 'z') {
-        risultato = c - 32;
-        return risultato;
-    }
-}
 
 int main() {
     char input;
     char valore;
-    cout << "Inserisci il carattere da convertire:" << endl;
     cin >> input;
     if (controllo(input)) {
         valore = convertitore(input);
-        cout << " " << valore;
+        cout << valore;
     } else {
         cout << "errore" << endl;
     }
