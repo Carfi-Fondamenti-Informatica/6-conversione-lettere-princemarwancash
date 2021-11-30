519c7bbe7e34f74e549d5db45e3e46da8cce3c06
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-11-30 22:15:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
+#include "lib.h"
 using namespace std;
-
-bool controllo (char c){
-    if (c >='A' and c <='Z'){
-        return true;
-    } else if(c >='a' and c<='z'){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-char convertitore(char c) {
-    char risultato;
-    if (c >= 'A' and c <= 'Z') {
-        risultato = c + 32;
-    } else if (c >= 'a' and c <= 'z') {
-        risultato = c - 32;
-        return risultato;
-    }
-}
 
 int main() {
     char input;
@@ -30,7 +11,7 @@ int main() {
         valore = convertitore(input);
         cout << "Il carattere ottenuto e\':" << " " << valore;
     } else {
-        cout << "errore" << endl;
+        cout << "Errore" << endl;
     }
     return 0;
 }
